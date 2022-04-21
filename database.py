b77aa776d430d2a7c59a46b5a4acad32acf69a95
Andrew Mathenge <mathenge@gmail.com>
2022-04-21 13:42:56 +0300

    updated database.py - 21APR2022

--- database.py
+++ database.py
@@ -1,14 +1,18 @@
+'''
+Flask sqlite database function.
+'''
+
 from flask import g
 import sqlite3
 
+db = 'message.db'
 
 def connect_db():
     # remember to use the following line in production
     # sql = sqlite3.connect('/home/fairacresltd/sms/message.db')
-    sql = sqlite3.connect('message.db')
+    sql = sqlite3.connect(db)
     sql.row_factory = sqlite3.Row
     return sql
-
 
 def get_db():
     if not hasattr(g, 'sqlite_db'):
